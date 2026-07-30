11fce958b7e5cf35374c7f1e55e77ae4f81e2d51
JohanAceroSalazar <johanacero2509@gmail.com>
2026-07-30 16:03:45 -0500

    Table correction, password reset tokens

--- 01_ddl/03_tables/002-table-Password-Reset-Tokens.sql
+++ 01_ddl/03_tables/002-table-Password-Reset-Tokens.sql
@@ -3,6 +3,8 @@ CREATE TABLE password_reset_tokens (
 
     id_user UUID NOT NULL,
 
+    token_identifier UUID NOT NULL UNIQUE,
+
     token_hash VARCHAR(255) NOT NULL,
 
     expires_at TIMESTAMP NOT NULL,
@@ -11,5 +13,10 @@ CREATE TABLE password_reset_tokens (
 
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT fk_password_reset_user
+        FOREIGN KEY (id_user)
+        REFERENCES users(id_user)
+        ON DELETE CASCADE
 );
