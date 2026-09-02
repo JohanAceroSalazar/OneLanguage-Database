907a9106e167acc8fb4ee1c3e0f945f6e76e02f2
JohanAceroSalazar <johanacero2509@gmail.com>
2026-09-02 15:18:36 -0500

    move tables users and password reset to schema auth

---
+++ 01_ddl/10_alter/001-move-users-and-password-reset-to-auth.sql
@@ -0,0 +1,2 @@
+ALTER TABLE public.users SET SCHEMA auth;
+ALTER TABLE public.password_reset_tokens SET SCHEMA auth;
---
+++ 05_rollbacks/01_ddl/10_alter/001-rollback-users-and-password-reset.sql
@@ -0,0 +1,2 @@
+ALTER TABLE auth.users SET SCHEMA public;
+ALTER TABLE auth.password_reset_tokens SET SCHEMA public;
